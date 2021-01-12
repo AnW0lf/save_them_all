567cde6c61a241d0e71c7c4848bf2e4766a35a35
AnW0lf <AngryWolfgang@gmail.com>
2021-01-12 18:16:43 +0300

    Initial commit

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    [SerializeField] private Color _sphereColor = Color.cyan;
+    [SerializeField] private GameObject _minionPrefab = null;
+    [SerializeField] private Transform[] _points = null;
+
+    private void OnDrawGizmos()
+    {
+        if (_points == null) return;
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Gizmos.color = _sphereColor;
+            Gizmos.DrawSphere(_points[i].position, 0.1f);
+        }
+    }
+
+    public void InstantiateMinions(int count)
+    {
+        count = Mathf.Clamp(count, 0, _points.Length);
+
+        for(int i = 0; i < count; i++)
+        {
+            GameObject minion = Instantiate(_minionPrefab);
+            minion.transform.position = _points[i].position;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class Utils
+{
+    public static Process GetCrossFade<A>(A from, A to, float duration, Action<A> setter, Func<A, A, float, A> lerp)
+    {
+        Process process = new Process();
+        IEnumerator crossFading = CrossFading(from, to, duration, setter, lerp, process.Coroutine);
+        process.Enumerator = crossFading;
+        return process;
+    }
+
+    public static IEnumerator CrossFading<A>(A from, A to, float duration, Action<A> setter, Func<A, A, float, A> lerp, Coroutine self)
+    {
+        float timer = 0f;
+
+        while (timer <= duration)
+        {
+            timer += Time.deltaTime;
+            setter(lerp(from, to, timer / duration));
+            yield return null;
+        }
+
+        self = null;
+    }
+}
+
+public class Process : MonoBehaviour
+{
+    public Coroutine Coroutine { get; private set; }
+    public IEnumerator Enumerator { get; set; }
+
+    public Process()
+    {
+        Coroutine = null;
+        Enumerator = null;
+    }
+
+    public Process(IEnumerator enumerator)
+    {
+        Coroutine = null;
+        Enumerator = enumerator;
+    }
+
+    public void Begin()
+    {
+        if (Coroutine != null) StopCoroutine(Coroutine);
+        Coroutine = StartCoroutine(Enumerator);
+    }
+
+    public void Break()
+    {
+        if (Coroutine != null) StopCoroutine(Coroutine);
+        Coroutine = null;
+    }
+}
